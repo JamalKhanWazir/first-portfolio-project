65cfaf89756d60ffafa0acd72b9c5bb2afe8d654
jamalkhanwazir <jamalkhanwaxir@gmail.com>
2023-08-13 00:20:05 +0500

    contact component error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import "./styles/app.scss";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Services from "./components/Services";
 
 function App() {
